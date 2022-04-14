ef6e8673f6cc79e5dbb333781ae0cc020d74c6c5
Michael Carr <xyeres@me.com>
2022-04-13 17:03:33 -0700

    Add gesturehandlerrootview

--- navigation/index.js
+++ navigation/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Routes from './Routes';
 import {store} from '../redux/store';
 import {Provider as ReduxProvider} from 'react-redux';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export default function Providers() {
   return (
-    <ReduxProvider store={store}>
-      <Routes />
-    </ReduxProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <ReduxProvider store={store}>
+        <Routes />
+      </ReduxProvider>
+    </GestureHandlerRootView>
   );
 }
